f3e28d0eda1bd3d7a0cc9c4964577ed7bb6a23a9
AAMCortez <aamcortez@gmail.com>
2024-08-14 15:59:55 +0100

    Navigation v1.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Fragment } from "react";
 import StartingPageContent from "../components/starting-page";
+import MainNavigation from "@/components/main-navigation";
 
 export default function Home() {
-  return (
-    <StartingPageContent />
-  );
+   return (
+      <Fragment>
+         <MainNavigation />
+         <StartingPageContent />
+      </Fragment>
+   );
 }
---
+++ src/components/main-navigation/index.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import React from "react";
+
+type Props = {};
+let session = true;
+
+export default function MainNavigation({}: Props) {
+   return (
+      <header className="w-full h-20 bg-cyan-700 shadow-md flex justify-between items-center px-[10%]">
+         <Link className="no-underline text-white font-bold" href="/">
+            <div className="font-lato text-2xl text-white m-0">Home</div>
+         </Link>
+         <nav>
+            <ul className="list-none m-0 p-0 flex items-baseline">
+               {!session && (
+                  <li className="m-0 mx-4">
+                     <Link className="no-underline text-white font-bold hover:text-red-900" href="/auth">Login</Link>
+                  </li>
+               )}
+               {session && (
+                  <li className="m-0 mx-4">
+                     <Link className="no-underline text-white font-bold hover:text-red-900" href="/profile">Profile</Link>
+                  </li>
+               )}
+               {session && (
+                  <li className="m-0 mx-4">
+                     <button className="font-inherit bg-transparent border border-white text-white font-bold py-2 px-6 rounded-lg cursor-pointer">
+                        Logout
+                     </button>
+                  </li>
+               )}
+            </ul>
+         </nav>
+      </header>
+   );
+}
