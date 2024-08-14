4ec0f6d9fe694596cec0337867bcdf72d877c5b2
AAMCortez <aamcortez@gmail.com>
2024-08-14 18:09:11 +0100

    folder structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import MainNavigation from "./components/main-navigation";
+import MainNavigation from "./components/main-navigation/main-navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +19,7 @@ export default function RootLayout({
       <html lang="en">
          <body className={inter.className}>
             <MainNavigation />
-            {children}
+            <main>{children}</main>
          </body>
       </html>
    );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import { Fragment } from "react";
 import StartingPageContent from "./components/starting-page";
 
-
 export default function Home() {
-   return (
-      <Fragment>
-         <StartingPageContent />
-      </Fragment>
-   );
+   return <StartingPageContent />;
 }
